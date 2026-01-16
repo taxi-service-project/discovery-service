e8aa9cb874457ef935c114def6a0d89d711e428e
201912025 <michi2012@naver.com>
2026-01-16 13:05:40 +0900

    feat: 스레드 전환 구간에서 확실한 전파 위해 Hooks 설정 명시적으로 추가

--- src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
+++ src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
@@ -3,12 +3,15 @@ package com.example.discovery_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
